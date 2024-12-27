60814c0c4afa70c1dac641ee2195a050fff45bc4
Kumar Adarsh <adarsh3128@gmail.com>
2024-12-27 17:16:13 +0530

    Create docker_push.groovy

---
+++ vars/docker_push.groovy
@@ -0,0 +1,10 @@
+def callString Project, String ImageTag, String dockerHubUser){
+           echo "Pushing the image to DockerHub"
+            withCredentials([usernamePassword(credentialsId: "dockerHubCred", passwordVariable: "dockerHubPass", usernameVariable: "dockerHubUser")]) {
+    sh """
+        echo ${env.dockerHubPass} | docker login -u ${env.dockerHubUser} --password-stdin
+    """
+             sh "docker image tag notes-app:latest ${env.dockerHubUser}/notes-app:latest"
+             sh "docker push ${env.dockerHubUser}/notes-app:latest"
+             }
+}
